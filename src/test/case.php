94a4d58929e468f18431a0ff64c655dd011afcba
fh <fh@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2005-09-08 08:59:50 +0000

    Handy assert methods for properties
    git-svn-id: http://svn.ez.no/svn/ezcomponents/packages/UnitTest/trunk@399 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

--- src/test/case.php
+++ src/test/case.php
@@ -8,6 +8,37 @@ class ezcTestCase extends PHPUnit2_Framework_TestCase
     {
         parent::__construct( $string );
     }
+
+    /**
+     * Checks if $expectedValues are properly set on $propertyName in $object.
+     */
+    public function assertProperty( $object, $expectedValues, $propertyName )
+    {
+        foreach( $expectedValues as $value )
+        {
+            $object->$propertyName = $value;
+            $this->assertEquals( $value, $object->$propertyName );
+        }
+    }
+
+    /**
+     * Checks if $setValues fail when set on $propertyName in $object.
+     * Setting the property must result in an exception.
+     */
+    public function assertPropertyFails( $object, $setValues, $propertyName )
+    {
+        foreach( $setValues as $value )
+        {
+            try
+            {
+                $object->$propertyName = $value;
+            }
+            catch( Exception $e ){
+                return;
+            }
+            $this->fail( "Setting property $propertyName to $value did not fail." );
+        }
+    }
 }
 
 
