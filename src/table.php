768e883648bfe4fc58b82eb72e55e3b349cb45f7
ts <ts@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2005-10-26 09:17:47 +0000

    - Doc update.
    git-svn-id: http://svn.ez.no/svn/ezcomponents/packages/ConsoleTools/trunk@731 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

--- src/table.php
+++ src/table.php
@@ -3,9 +3,10 @@
  * File containing the ezcConsoleTable class.
  *
  * @package ConsoleTools
- * @version //autogen//
+ * @version //autogentag//
  * @copyright Copyright (C) 2005 eZ systems as. All rights reserved.
  * @license LGPL {@link http://www.gnu.org/copyleft/lesser.html}
+ * @filesource
  */
 
 /**
