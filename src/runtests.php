22d0bb38db91161c809c9eaadee4808d1b9592df
amos <amos@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2005-11-07 03:54:06 +0000

    - Added support for PEAR style class names in autoloader, it looks for an   underscore in the class name. If found it will replace all of them with a   slash.
    git-svn-id: http://svn.ez.no/svn/ezcomponents/packages/UnitTest/trunk@902 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

--- src/runtests.php
+++ src/runtests.php
@@ -3,6 +3,14 @@
 function __autoload( $class_name )
 {
     require_once("Base/trunk/src/base.php");
+    if ( strpos( $class_name, "_" ) !== false )
+    {
+        $file = str_replace( "_", "/", $class_name ) . ".php";
+        $val = require_once( $file );
+        if ( $val == 0 )
+            return true;
+        return false;
+    }
     ezcBase::autoload( $class_name );
 }
 
