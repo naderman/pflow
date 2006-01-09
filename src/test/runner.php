4dba62aa5bb09ee58972b154d46defa4063ab270
dr <dr@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2006-01-09 13:07:04 +0000

    - Those are not PDO implementations, but our implementations.
    git-svn-id: http://svn.ez.no/svn/ezcomponents/packages/UnitTest/trunk@1745 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

--- src/test/runner.php
+++ src/test/runner.php
@@ -288,7 +288,7 @@ class ezcTestRunner extends PHPUnit2_TextUI_TestRunner
                 case ezcDbException::MISSING_DATABASE_NAME: $this->printError( "The database name is missing." ); break;
                 case ezcDbException::MISSING_USER_NAME: $this->printError( "The username is missing." ); break;
                 case ezcDbException::MISSING_PASSWORD: $this->printError( "The password is missing." ); break;
-                case ezcDbException::UNKNOWN_IMPL: $this->printError( "Unknown PDO implementation. Make sure you specified an existing driver (mysql, pgsql, oci) and that your PHP version has the modules (php -m): PDO and pdo_<driver> (e.g. pdo_mysql, pdo_pgsql, etc)." ); break;
+                case ezcDbException::UNKNOWN_IMPL: $this->printError( "Unknown database implementation. Make sure you specified an existing driver (mysql, pgsql, oci) and that your PHP version has the modules (php -m): PDO and pdo_<driver> (e.g. pdo_mysql, pdo_pgsql, etc)." ); break;
                 case ezcDbException::INSTANCE_NOT_FOUND: $this->printError( "Cannot find the db instance." ); break;
                 case ezcDbException::NOT_IMPLEMENTED: $this->printError( "The functionality is not implemented." ); break;
                 default: $this->getTraceAsString(); break;
