61678261a2f82812560ecfbaace76721a36a3260
ts <ts@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2006-01-18 09:21:55 +0000

    - Added example on how to include PHP sources to RST. - Added first example skeleton. - Added common autoload include for examples.
    git-svn-id: http://svn.ez.no/svn/ezcomponents/packages/ConsoleTools/trunk@1953 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

---
+++ docs/tutorial_autoload.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Autoload ezc classes 
+ * 
+ * @param string $class_name 
+ */
+function __autoload( $class_name )
+{
+    if ( ezcBase::autoload( $class_name ) )
+    {
+        return;
+    }
+    if ( strpos( $class_name, '_' ) !== false )
+    {
+        $file = str_replace( '_', '/', $class_name ) . '.php';
+        require_once( $file );
+    }
+}
+
+?>
---
+++ docs/tutorial_example_01.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once './tutorial_autoload.php';
