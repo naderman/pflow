af04021fdd485be9114db0eca4275d92bf32cf54
ts <ts@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2006-01-18 13:36:22 +0000

    - Adding example sources.
    git-svn-id: http://svn.ez.no/svn/ezcomponents/packages/ConsoleTools/trunk@1963 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

--- docs/tutorial_example_01.php
+++ docs/tutorial_example_01.php
@@ -3,3 +3,9 @@
 require_once 'tutorial_autoload.php';
 
 $output = new ezcConsoleOutput();
+
+$output->formats->info->color = 'blue';
+
+$output->outputText( 'Test text in color blue', 'info' );
+
+?>
