d91b32c6cc8e4f305705df0b8dfe03dbc564bab4
dr <dr@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2006-01-23 10:33:13 +0000

    - We don't need PEAR autoloading in the tutorials.
    git-svn-id: http://svn.ez.no/svn/ezcomponents/packages/ConsoleTools/trunk@2001 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

--- docs/tutorial_autoload.php
+++ docs/tutorial_autoload.php
@@ -13,11 +13,5 @@ function __autoload( $class_name )
     {
         return;
     }
-    if ( strpos( $class_name, '_' ) !== false )
-    {
-        $file = str_replace( '_', '/', $class_name ) . '.php';
-        require_once( $file );
-    }
 }
-
 ?>
