095e03c14444c03b049e6b18ff5a98c48b26c5dc
dr <dr@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2006-01-29 11:24:50 +0000

    - Make this consistent...
    git-svn-id: http://svn.ez.no/svn/ezcomponents/packages/ConsoleTools/trunk@2079 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

--- docs/tutorial_autoload.php
+++ docs/tutorial_autoload.php
@@ -1,17 +1,13 @@
 <?php
-
 require_once 'Base/trunk/src/base.php';
 
 /**
  * Autoload ezc classes 
  * 
- * @param string $class_name 
+ * @param string $className 
  */
-function __autoload( $class_name )
+function __autoload( $className )
 {
-    if ( ezcBase::autoload( $class_name ) )
-    {
-        return;
-    }
+    ezcBase::autoload( $className );
 }
 ?>
