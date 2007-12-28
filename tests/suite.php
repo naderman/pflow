ea549ea09fa9072532f24aab7607a84059536adf
sb <sb@bc0e7bdc-f0fc-0310-8ff6-f601c06e1256>
2007-12-28 16:25:12 +0000

    - dos2unix - svn propset svn:eol-style native - svn propset svn:keywords Id - Clean up test suite.
    git-svn-id: http://svn.ez.no/svn/ezcomponents/experimental/Reflection@7040 bc0e7bdc-f0fc-0310-8ff6-f601c06e1256

--- tests/suite.php
+++ tests/suite.php
@@ -1,37 +1,31 @@
 <?php
 /**
- * @copyright Copyright (C) 2005, 2006 eZ systems as. All rights reserved.
- * @license http://ez.no/licenses/new_bsd New BSD License
- * @version //autogentag//
- * @filesource
- * @package Reflection
+ * @package Workflow
  * @subpackage Tests
+ * @version //autogentag//
+ * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
  */
 
-/**
- * Require the test cases
- */
-require_once 'extended_reflection_test.php';
-require_once 'function_test.php';
-require_once 'parameter_test.php';
 require_once 'class_test.php';
 require_once 'class_external_test.php';
-require_once 'method_test.php';
-require_once 'property_test.php';
+require_once 'extended_reflection_test.php';
 require_once 'extension_test.php';
+require_once 'function_test.php';
+require_once 'method_test.php';
+require_once 'parameter_test.php';
+require_once 'parser_test.php';
+require_once 'property_test.php';
+require_once 'tag_factory_test.php';
 require_once 'type_factory_test.php';
 require_once 'type_mapper_test.php';
-require_once 'parser_test.php';
-require_once 'tag_factory_test.php';
 
-/** Test Subjects */
-require_once 'test_classes/webservice.php';
+require_once 'test_classes/functions.php';
 require_once 'test_classes/methods.php';
 require_once 'test_classes/methods2.php';
-require_once 'test_classes/functions.php';
 require_once 'test_classes/MyReflectionClass.php';
+require_once 'test_classes/webservice.php';
 
-/** Helper Lib */
 require_once 'test_helper.php';
 
 /**
@@ -43,26 +37,25 @@ class ezcReflectionSuite extends PHPUnit_Framework_TestSuite
     public function __construct()
     {
         parent::__construct();
-        $this->setName("Reflection");
+        $this->setName('Reflection');
 
-        $this->addTest( ezcReflectionDocTagFactoryTest::suite() );
-        $this->addTest( ezcReflectionDocParserTest::suite() );
-
-        $this->addTest( ezcReflectionTest::suite() );
-        $this->addTest( ezcReflectionFunctionTest::suite() );
-        $this->addTest( ezcReflectionParameterTest::suite() );
         $this->addTest( ezcReflectionClassTest::suite() );
         $this->addTest( ezcReflectionClassExternalTest::suite() );
-        $this->addTest( ezcReflectionMethodTest::suite() );
-        $this->addTest( ezcReflectionPropertyTest::suite() );
+        $this->addTest( ezcReflectionTest::suite() );
         $this->addTest( ezcReflectionExtensionTest::suite() );
+        $this->addTest( ezcReflectionFunctionTest::suite() );
+        $this->addTest( ezcReflectionMethodTest::suite() );
+        $this->addTest( ezcReflectionParameterTest::suite() );
+        $this->addTest( ezcReflectionPropertyTest::suite() );
+        $this->addTest( ezcReflectionDocParserTest::suite() );
+        $this->addTest( ezcReflectionDocTagFactoryTest::suite() );
         $this->addTest( ezcReflectionTypeFactoryTest::suite() );
         $this->addTest( ezcReflectionTypeMapperTest::suite() );
     }
 
     public static function suite()
     {
-        return new self();
+        return new ezcReflectionSuite;
     }
 }
 ?>
